5e5373c9887e377e7f399e6b35a989c3ef54d0a3
Guilherme Vieira <137854660+gvieira1@users.noreply.github.com>
2025-05-28 16:45:55 -0300

    novo build jar

--- src/main/java/br/ifsp/edu/feedback/model/User.java
+++ src/main/java/br/ifsp/edu/feedback/model/User.java
@@ -12,12 +12,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users") 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
